de020419479ec0105171e27c911782a2c3b8b225
Julien Capellari <julien@capellari.net>
2020-09-08 23:09:02 +0200

    update: openable minimap

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,17 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Layer } from 'src/maps/layer';
 
 import AppLayout from './components/layout/AppLayout';
-import Map from './components/map/Map';
-import MiniMap from 'src/components/map/MiniMap';
-import { Box } from '@material-ui/core';
-import { Vector } from 'src/utils/math2d';
+import Navigator from './components/Navigator';
 
 // Styles
 const useStyle = makeStyles(({ spacing }) => ({
-  container: {
-    position: 'relative',
-
+  navigator: {
     flex: 1
-  },
-  map: {
-    height: '100%'
-  },
-  miniMap: {
-    position: 'absolute',
-    top: spacing(2),
-    right: spacing(2),
-    zIndex: 10,
-    height: 100,
-    width: 100,
-
-    borderRadius: '50%',
-    overflow: 'hidden',
   }
 }));
 
 // Component
 const App = () => {
-  // State
-  const [center, setCenter] = useState<Vector>({ x: -5, y: -5 });
-
   // Memo
   const layer = useMemo(() => Layer.generate(20, 20, 'rock'), []);
 
@@ -46,15 +24,7 @@ const App = () => {
 
   return (
     <AppLayout>
-      <section className={styles.container}>
-        <article className={styles.map}>
-          <Map center={center} layer={layer} />
-        </article>
-
-        <Box className={styles.miniMap} boxShadow={2}>
-          <MiniMap center={center} layer={layer} />
-        </Box>
-      </section>
+      <Navigator className={styles.navigator} layer={layer} />
     </AppLayout>
   );
 };
---
+++ src/components/Navigator.tsx
@@ -0,0 +1,83 @@
+import React, { FC, useState } from 'react';
+import clsx from 'clsx';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import { Layer } from 'src/maps/layer';
+
+import Map from './map/Map';
+import MiniMap from './map/MiniMap';
+
+// Styles
+const useStyle = makeStyles(({ spacing, shadows, transitions }) => ({
+  container: {
+    position: 'relative'
+  },
+  map: {
+    height: '100%'
+  },
+  miniMap: {
+    position: 'absolute',
+    top: spacing(2),
+    right: spacing(2),
+    zIndex: 10,
+    height: 100,
+    width: 100,
+
+    cursor: 'pointer',
+    boxShadow: shadows[2],
+    borderRadius: 50,
+    overflow: 'hidden',
+
+    transition: transitions.create(
+      ['top', 'right', 'height', 'width', 'border-radius'],
+      { duration: 600 }
+    ),
+
+    '&.open': {
+      top: 0,
+      right: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: 0
+    }
+  }
+}));
+
+// Types
+export interface NavigatorProps {
+  layer: Layer;
+  className?: string;
+}
+
+// Component
+const Navigator: FC<NavigatorProps> = (props) => {
+  const {
+    layer,
+    className
+  } = props;
+
+  // State
+  const [center, setCenter] = useState({ x: 0, y: 0 });
+  const [openMiniMap, setOpenMiniMap] = useState(false);
+
+  // Render
+  const styles = useStyle();
+
+  return (
+    <div className={clsx(styles.container, className)}>
+      <div className={styles.map}>
+        <Map center={center} layer={layer} />
+      </div>
+
+      <div
+        className={clsx(styles.miniMap, { open: openMiniMap })}
+        onClick={() => setOpenMiniMap(!openMiniMap)}
+      >
+        <MiniMap center={center} layer={layer} />
+      </div>
+    </div>
+  );
+};
+
+export default Navigator;
