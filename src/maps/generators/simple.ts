6648e3e6a61848eda8ca392fff588acd4a18066a
Julien Capellari <julien.capellari@neoxia.com>
2020-09-28 18:14:15 +0200

    fix: simple generator

--- src/maps/generators/simple.ts
+++ src/maps/generators/simple.ts
@@ -1,11 +1,11 @@
 import { BiomeName } from 'src/biomes';
 import { ISize, Vector } from 'src/utils/math2d';
 
-import { Layer, Tile } from '../layer';
+import { InputTile, Layer } from '../layer';
 
 // Generator
 export function simpleLayer(size: ISize, biome: BiomeName): Layer {
-  const tiles: Tile[] = [];
+  const tiles: InputTile[] = [];
 
   for (let y = 0; y < size.h; ++y) {
     for (let x = 0; x < size.w; ++x) {
